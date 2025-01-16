8a7b71bb8ee8e982ea4520eefc45b7ec5f1f491e
only4arindam1 <only4arindam@gmail.com>
2025-01-15 22:31:42 -0500

    Updated with Junit -5

---
+++ src/test/java/io/flowsquad/camunda/test/SimpleTestCase.java
@@ -0,0 +1,35 @@
+package io.flowsquad.camunda.test;
+
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension;
+
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.assertj.core.api.Assertions.*;
+
+
+@ExtendWith(ProcessEngineExtension.class)
+@Deployment(resources = {"testProcess.bpmn"})
+public class SimpleTestCase {
+
+    @Test
+    public void shouldExecuteProcess() {
+        // Given we create a new process instance
+        ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess");
+        // Then it should be active
+        assertThat(processInstance).isActive();
+        // And it should be the only instance
+        assertThat(processInstanceQuery().count()).isEqualTo(1);
+        // And there should exist just a single task within that process instance
+        assertThat(task(processInstance)).isNotNull();
+
+        // When we complete that task
+        complete(task(processInstance));
+        // Then the process instance should be ended
+        assertThat(processInstance).isEnded();
+    }
+
+}
